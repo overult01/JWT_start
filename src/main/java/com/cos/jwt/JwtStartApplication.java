e14632304e1859aee10a4aee720eda02f5300d87
it_st it <overult01@gmail.com>
2022-06-03 10:13:20 +0900

    first

---
+++ src/main/java/com/cos/jwt/JwtStartApplication.java
@@ -0,0 +1,13 @@
+package com.cos.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtStartApplication.class, args);
+	}
+
+}
